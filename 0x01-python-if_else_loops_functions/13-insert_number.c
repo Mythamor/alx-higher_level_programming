42197a2413058e3d658c1912f2ccb5afe203284f
Mythamor <bethmithamo.bm@gmail.com>
2023-05-09 15:29:34 -0700

    Insert in sorted linked list

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,70 +1,47 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_node - malloc and insert node into sorted singly linked list
- * @head: pointer to head of linked list
- * @number: data for new node
- * Return: address of new node, or NULL if failed
+ * insert_node - inserts a new node so that linked list remains sorted
+ * @head: pointer to the beginning of linked list
+ * @number: value for n
+ * Return: address of new node or NULL if fails
  */
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *tmp = NULL;
-	listint_t *new = NULL;
+	listint_t *new, *hold = *head;
+	unsigned int i = 0;
 
-	if (!head)
-		return (NULL);
-
-	/* malloc new node */
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
-	new->next = NULL;
-
-	/* if no linked list, insert node as the only member */
-	if (*head == NULL)
+	if (!(hold) || (*hold).n > number) /* add to beginning of linked list*/
 	{
+		new = malloc(sizeof(listint_t));
+		if (!new)
+			return (NULL);
+
+		(*new).n = number;
+		(*new).next = *head;
+
 		*head = new;
-		(*head)->next = NULL;
-		return (new);
-	}
-	/* if only one node in linked list, do comparision and insert */
-	if ((*head)->next == NULL)
-	{
-		if ((*head)->n < new->n)
-			(*head)->next = new;
-		else
-		{
-			new->next = *head;
-			*head = new;
-		}
-		return (new);
+
+		return (*head);
 	}
 
-	/* if lots of nodes in linked list, do comparision and insert */
-	tmp = *head;
-	while (tmp->next != NULL)
+	while (hold)
 	{
-		/* if new node num is smaller than first node, insert */
-		if (new->n < tmp->n)
+		if (!((*hold).next) || (*hold).next->n > number)
 		{
-			new->next = tmp;
-			*head = new;
+			new = malloc(sizeof(listint_t));
+			if (!new)
+				return (NULL);
+			(*new).n = number;
+			(*new).next = (*hold).next;
+			(*hold).next = new;
 			return (new);
 		}
-		/* if new node num is the same as an existing node, insert */
-		/* compare previous node and next node, insert in between */
-		if (((new->n > tmp->n) && (new->n < (tmp->next)->n)) ||
-		    (new->n == tmp->n))
-		{
-			new->next = tmp->next;
-			tmp->next = new;
-			return (new);
-		}
-		tmp = tmp->next;
+		hold = (*hold).next;
+		i++;
 	}
-	/* if new node is greatest and never inserted, insert now */
-	tmp->next = new;
-	return (new);
+
+	return (NULL);
 }
