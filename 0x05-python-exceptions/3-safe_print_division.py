aa953e97884076ebaea9ff5731d175716ab0d65d
Mythamor <bethmithamo.bm@gmail.com>
2023-05-29 13:24:26 -0700

    Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except:
+        pass
+    finally:
+        try:
+            print("Inside result: {}".format(result))
+        except:
+            print("Inside result: {}".format(None))
+            return None
+    return result
