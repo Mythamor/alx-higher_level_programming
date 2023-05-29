95b018226501920d26baedee699ce5e6948d8a5e
Mythamor <bethmithamo.bm@gmail.com>
2023-05-29 13:33:15 -0700

    Integers division with debug

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -2,12 +2,12 @@
 def safe_print_division(a, b):
     try:
         result = a / b
-    except:
+    except ZeroDivisionError:
         pass
     finally:
         try:
             print("Inside result: {}".format(result))
-        except:
+        except NameError:
             print("Inside result: {}".format(None))
             return None
     return result
