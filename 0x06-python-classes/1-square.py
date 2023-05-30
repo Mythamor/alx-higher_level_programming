4953fefc88c3f128cbacfe57308312aa7ddcb3a6
Mythamor <bethmithamo.bm@gmail.com>
2023-05-30 06:52:46 -0700

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module 1-square
+    Defines the size of a square as a private instance attribute
+"""
+
+
+class Square:
+    def __init__(self, size):
+        self.__size = size
