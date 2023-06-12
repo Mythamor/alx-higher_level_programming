ad50354a6a057467b61ccf03b654af02208a0e7d
Mythamor <bethmithamo.bm@gmail.com>
2023-06-12 14:06:01 -0700

    My List

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 """
+Module 1-my_list
+
 class MyList that inherits from list
+return sorted list
 """
 
 
 class MyList(list):
-    """class that inherits from list"""
+    """class that inherits from list
+        
+        Methods:
+        print_sorted(self)
+    """
+
+
     def print_sorted(self):
         """ print sorted list"""
         print(sorted(self))
