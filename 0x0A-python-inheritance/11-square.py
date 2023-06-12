62c774a9746996e137c28f50d2608feb57360d52
Mythamor <bethmithamo.bm@gmail.com>
2023-06-12 16:04:09 -0700

    Square #2

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -10,7 +10,8 @@ with instantiation of private attributes width and height, validated by parent,
 extends parent's area method and prints with __str__
 
 Contains subclass Square
-with instantiation of private attribute size, validated by superclass
+with instantiation of private attribute size, validated by superclass,
+and prints with __str__
 """
 
 
@@ -21,6 +22,7 @@ class Square(Rectangle):
     """inherits from Rectangle, who inherits from BaseGeometry
     methods:
         __init__(self, size)
+        __str__(self)
     """
     def __init__(self, size):
         """initializes size
@@ -30,3 +32,8 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+
+    def __str__(self):
+        """prints [Rectangle] <width>/<height>"""
+        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                                         self.__size, self.__size)
