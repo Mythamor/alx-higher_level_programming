9eed2af80ef36ce305e90a4557f1478f726cfe11
Mythamor <bethmithamo.bm@gmail.com>
2023-08-15 16:22:02 -0700

    Reversed List

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-	let count = 0;
-	for (let l in list) {
-		if (list[l] === searchElement) {
-			count++;
-		}
-	}
-	return count;
+  let count = 0;
+  for (const l in list) {
+    if (list[l] === searchElement) {
+      count++;
+    }
+  }
+  return count;
 };
