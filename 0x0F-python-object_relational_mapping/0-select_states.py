653ea671167dae6ca30c6f214b137e5c93aef125
Mythamor <bethmithamo.bm@gmail.com>
2023-08-20 14:28:59 +0000

    Select all query

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 	#create a cursor to exec queries using SQL
 	cur=conn.cursor()
 	cur.execute("SELECT * FROM states ORDER BY id ASC")
-	query_rows = cur.fetchall
+	query_rows = cur.fetchall()
 	for row in query_rows:
 		print(row)
 	cur.close()
