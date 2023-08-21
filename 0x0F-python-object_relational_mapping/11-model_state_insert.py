16d0508f5b6673c0ba2d930ec8e3692422c1c03e
Mythamor <bethmithamo.bm@gmail.com>
2023-08-21 03:11:24 +0300

    sqlalchemy-Add a new state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-    engine =  create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(user,
-                            passwd, db), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(user,
+                           passwd, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     # create session
