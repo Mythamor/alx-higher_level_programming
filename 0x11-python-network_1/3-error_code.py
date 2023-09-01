d11cb78087791dee22682ea6c4b3c29e792ca599
Mythamor <bethmithamo.bm@gmail.com>
2023-09-02 00:48:15 +0300

    Error code #0

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""script takes in URL, sends request to URL & displays body
+of response (decoded in utf-8)
+"""
+
+
+import urllib.parse
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        with open urllib.request.urlopne(url) as response:
+            print(response.read().decode("utf-8"))
+        except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e.code))
