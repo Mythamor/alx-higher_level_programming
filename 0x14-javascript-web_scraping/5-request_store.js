4fc2d5945f7800f2a33abf7e078b1a42b604008f
Mythamor <bethmithamo.bm@gmail.com>
2023-09-27 01:04:16 +0300

    script that gets the contents of a webpage and stores it in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,14 +1,19 @@
 #!/usr/bin/node
 
-let url = process.argv[2];
-let filename = process.argv[3];
+const url = process.argv[2];
+const filename = process.argv[3];
 const fs = require('fs');
 const request = require('request');
 
-request(url, function (err, response, body) {
+request(url, (err, response, body) => {
   if (err) {
     console.log(err);
-  } else {
-    fs.writeFile(filename, body, 'utf8');
+    return;
   }
+
+  fs.writeFile(filename, body, { encoding: 'utf-8' }, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 });
