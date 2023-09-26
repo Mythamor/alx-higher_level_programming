541f26d575efd1e6940ce8cf66c729dac51d4f83
Mythamor <bethmithamo.bm@gmail.com>
2023-09-26 22:39:03 +0300

    a script that display the status code of a GET request.

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,6 +3,10 @@
 const url = process.argv[2];
 const request = require('request');
 
-request(url, (response, body) => {
-  console.log('code: ' + response.statusCode);
+request(url, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
 });
